8ce8252180c443b03ab1bcf0a10178a9e1db6fe1
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-07-29 18:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+// script.js
+
+// Select the main photo element
+const mainPhoto = document.querySelector('.photo-main img');
+
+// Select all variant images
+const variantImages = document.querySelectorAll('.variant ul li img');
+
+
+variantImages.forEach(img => {
+  img.addEventListener('click', () => {
+    
+    mainPhoto.src = img.src;
+    mainPhoto.alt = img.alt;
+  });
+});
+
+// Add to cart button functionality
+const addToCartBtn = document.querySelector('.add-to-cart');
+addToCartBtn.addEventListener('click', () => {
+  alert('Apple added to cart!');
+});
